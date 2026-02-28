1dd87f5d4213928d8d7b8456f74e5d80c561341a
Dmirtii <Zhdanof1@yandex.ru>
2026-02-28 12:49:22 +0300

    Added start content with animations

---
+++ Source/GameAnimationSample/CatComponents/WallRunComponet.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WallRunComponet.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GAMEANIMATIONSAMPLE_API UWallRunComponet : public UActorComponent
+{
+	GENERATED_BODY()
+
+};
